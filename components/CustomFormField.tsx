6edca6dd167b07b6a70fe42e6b9ba8c6bdbec7c5
Guilherme Mattos <guilhermevsmattos@outlook.com>
2024-08-22 20:45:24 -0300

    feat: creating select custom input

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -19,6 +19,7 @@ import { E164Number } from "libphonenumber-js/core";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 interface CustomProps {
   control: Control<any>;
   fieldType: FormFieldType;
@@ -104,6 +105,21 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
       );
     case FormFieldType.SKELETON:
       return renderSkeleton ? renderSkeleton(field) : null;
+    case FormFieldType.SELECT:
+      return (
+        <FormControl>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger className="shad-select-trigger">
+                <SelectValue placeholder={props.placeholder} />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent className="shad-select-content">
+              {props.children}
+            </SelectContent>
+          </Select>
+        </FormControl>
+      );
     default:
       break;
   }
